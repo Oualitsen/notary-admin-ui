d29a29d4e8ac67a731d2951d06102985c2c8de07
riro15 <riro1515@gmail.com>
2023-02-08 17:39:39 +0100

    Correction du code Form Convert to Map

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -68,7 +68,7 @@ class MyAppState extends State<MyApp> {
               builder: (context) {
                 switch (settings.name) {
                   case HomePage.home:
-                    return  HomePage();
+                    return HomePage();
 
                   case ProfilePage.routeName:
                     return const ProfilePage();
